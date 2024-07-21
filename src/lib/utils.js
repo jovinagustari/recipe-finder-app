08716cacb235adb6b104ef393274f42012b85068
Jovina J <jovinagustari17@gmail.com>
2024-07-21 18:26:33 +0700

    final

---
+++ src/lib/utils.js
@@ -0,0 +1,23 @@
+const COLORS = {
+    green: {
+        bg: "bg-[#ECF7D4]",
+        badge: "bg-[#D6F497",
+    },
+    orange: {
+        bg: "bg-[#F9EFE1]",
+        badge: "bg-[#F7E0B8]",
+    },
+    green: {
+        bg: "bg-[#FBE5E7]",
+        badge: "bg-[#FDC6C7]",
+    },
+};
+
+export const getRandomColor = () => {
+    const colorNames = Object.keys(COLORS); // Get array of the keys (color names)
+    const randomIndex = Math.floor(Math.random() * colorNames.length);
+    const randomColorName = colorNames[randomIndex];
+    return COLORS[randomColorName];
+}
+
+getRandomColor()
