75b2b51251469772da62896411bee2203cd72c24
Jovina J <jovinagustari17@gmail.com>
2024-07-21 11:35:22 +0700

    ui design completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import HomePage from "./pages/HomePage"
+import FavoritesPage from "./pages/FavoritesPage"
+
+const App = () => {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
